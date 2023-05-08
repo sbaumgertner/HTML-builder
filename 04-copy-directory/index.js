eaa46a1cec990be07b4e5f96f3898504f3597449
sbaumgertner <s.v.baumgertner@gmail.com>
2023-05-08 17:48:17 +0400

    fix: fix copy directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,14 +6,9 @@ exports.copyDirectory = copyDirectory;
 async function copyDirectory(src, dist){
     
     await fs.promises.mkdir(dist, { recursive: true });
-    const oldFiles = await fs.promises.readdir(dist, {withFileTypes: true});
+    const oldFiles = await fs.promises.readdir(dist);
     for (const file of oldFiles){
-        if (file.isDirectory()){
-            await fs.promises.rmdir(path.join(dist, file.name), { recursive: true });
-        }
-        else {
-            await fs.promises.unlink(path.join(dist, file.name));
-        }
+        await fs.promises.rm(path.join(dist, file), { recursive: true });
     }
 
     const files = await fs.promises.readdir(src, {withFileTypes: true});
